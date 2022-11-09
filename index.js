16b81b78c6332e7e19d131ca9cfebd26fa69a9e9
sergiomm84 <sergiomonterrubio@outlook.com>
2022-11-08 21:41:18 -0600

    Setting up files and express server

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+
+
+//app.use(express.json)
+app.use(express.static('public'))
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`Listening on port: ${port}`)})
+
+
+//console.log("hello world");
