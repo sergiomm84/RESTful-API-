b4fd4568e72698b49ac15e5b1ee3d27a3777dea5
sergiomm84 <sergiomonterrubio@outlook.com>
2022-11-10 02:56:12 -0600

    Agregados métodos GET,PUT,DELETE en routes

--- routes/customer.js
+++ routes/customer.js
@@ -11,7 +11,7 @@ router.post('/customer',(req,res)=>{
      const model =new CustomerModel(req.body)
      model.save()
      .then(doc => {
-        if(!doc ||  doc.length === 0){
+        if(!doc || doc.length === 0){
             return res.status(500).send(doc)
         }
         res.status(201).send(doc)
@@ -21,4 +21,52 @@ router.post('/customer',(req,res)=>{
      })
 })
 
+router.get('/customer',(req,res) =>{
+    if(!req.query.email){
+        return res.status(400).send("Missing URL parameter email")
+    }
+    
+    CustomerModel.findOne({
+        email: req.query.email
+    })
+    .then(doc =>{
+        res.json(doc)
+    })
+    .catch(err =>{
+        res.status(500).json(err);
+    })
+})
+router.put('/customer',(req,res) =>{
+    if(!req.query.email){
+        return res.status(400).send("Missing URL parameter email")
+    }
+    
+    CustomerModel.findOneAndUpdate({
+        email: req.query.email
+    },req.body, {
+        new:true
+    })
+    .then(doc =>{
+        res.json(doc)
+    })
+    .catch(err =>{
+        res.status(500).json(err);
+    })
+})
+
+router.delete('/customer',(req,res) =>{
+    if(!req.query.email){
+        return res.status(400).send("Missing URL parameter email")
+    }
+    
+    CustomerModel.findOneAndRemove({
+        email: req.query.email
+    })
+    .then(doc =>{
+        res.json(doc)
+    })
+    .catch(err =>{
+        res.status(500).json(err);
+    })
+})
 module.exports =router;
