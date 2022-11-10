82a41caa3a799ecf9c657bd27cf4824c861284b3
sergiomm84 <sergiomonterrubio@outlook.com>
2022-11-09 20:35:42 -0600

    Agregar parámetros a las rutas. Uno a través de req.params person/Minombre y otro con QueryStrings pperson?name=Minombre

--- routes/person.js
+++ routes/person.js
@@ -1,7 +1,29 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); ///
+/*
 router.get('/person', (req,res) =>{
-    res.send('You have requested PERSON');
-})
+    //res.send('You have requested PERSON');
+    res.send({param: "Hola"});
 
-module.exports = router;
+}) /// se define el nombre de la ruta
+
+router.get('/person/:id', (req,res) =>{
+    
+    res.send(`Routes person con variable ${req.params.id}`);
+
+}) /// se define el nombre de la ruta
+*/
+router.get('/person', (req,res) =>{
+    if(req.query.id){
+        res.send(`Routes person con variable ${req.query.id}`);
+    }
+    else{
+        res.send({param: "Hola"});
+    }
+    
+
+}) /// se define el nombre de la ruta
+
+
+router.get('/person')
+module.exports = router; ///exportar para poder usarlo en index.js
